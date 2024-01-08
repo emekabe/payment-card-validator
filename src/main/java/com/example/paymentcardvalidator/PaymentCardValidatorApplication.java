b12c38f2d12f8270aeec7d0b80b8c2f1ad9eb4d4
Chukwuemeka Chukwulobe <emekabe@yahoo.com>
2024-01-08 11:20:55 +0100

    Initial commit

---
+++ src/main/java/com/example/paymentcardvalidator/PaymentCardValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentcardvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCardValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCardValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paymentcardvalidator/PaymentCardValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentcardvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCardValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
